c3851abdbbe8868fefd9687f3e780f88a719148f
EC2 Default User <ec2-user@ip-172-31-91-142.ec2.internal>
2022-01-06 11:49:59 +0000

    disabledの追加

--- dentaku.js
+++ dentaku.js
@@ -1,12 +1,31 @@
+
+var calculations = document.getElementById("calculation");
 var result = document.getElementById("result");
+
+     calculations.addEventListener("click",function(){
+      calculations.setAttribute("disabled",true);
+    })
     function edit(elem) {
       result.value = result.value + elem.value;
     }
     function calc() {
       result.value = new Function("return " + result.value)();
+      calculations.removeAttribute("disabled",true);
     }
     function reset(){
       result.value = ""
     }
+    
+   
+   
+      
+    
+    
+    
+    
+    
+ 
+    
+   
  
  
